9718a9784a705c62c6369b9cc0566b284fe0b46d
Shahani Callueng <100544332+shahanic@users.noreply.github.com>
2022-05-03 23:55:08 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,44 +1,50 @@
+
 public class App {
-    /**
-     * This method takes in two int parameters to add.
-     * 
-     */
-    private static int sum2(int num1, int num2){
-        return num1 + num2;
-    }
 
     /**
-     * This method takes in 3 int parameters to add.
-     * 
+     * Modified the methods in such a way that the Class App
+     * can now have 2 overloaded computeSum() methods.
+     * One computing the sum of two numbers, and one computing the sum
+     * of three numbers.
      */
-    private static int sum3(int num1, int num2, int num3){
+    //This method takes in two int parameters to add.
+    private static int computeSum(int num1, int num2) {
+        return num1 + num2;
+    }
+    // This method takes in 3 int parameters to add.
+    private static int computeSum(int num1, int num2, int num3) {
         return num1 + num2 + num3;
     }
 
-    /**
-     * Modify the methods in such a way that the Class App
-     * can now have 2 overloaded computeSum() methods.
-     * One computing the sum of two numbers, and one computing the sum
-     * of three numbers. 
-     */
+     // method that returns the greater integer value
+    private static int greaterValue (int num1, int num2){
+        if (num1 > num2){
+            return num1;
+        }
+        else if (num1 < num2);{
+            return num2;
+        }
+    }
+    //method that returns the greater double value
+    private static double greaterValue ( double num1, double num2){
+        if (num1 > num2){
+            return num1;
+        }
+        else if (num1 < num2);{
+            return num2;
+        }
+
+    }
 
 
-    /**
-     * 
-     * Create method that returns the greater integer value
-     */
-
-     
-    /**
-     * 
-     * Create method that returns the greater double value
-     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        /**
-         * Create Method calls that takes in 2 parameters, and 3 parameters
-         * respectively and computes their sum.
-         */
+
+
+         // Method calls that takes in 2 parameters and 3 parameter respectively and computes their sum.
+
+        System.out.println("The sum of 23 + 43 is equal to " + computeSum(23,43));
+        System.out.println("The sum of 32 + 86 + 87 is equal to " + computeSum(32, 86,87));
 
         /**
          * Call the greaterValue method with integer parameters
